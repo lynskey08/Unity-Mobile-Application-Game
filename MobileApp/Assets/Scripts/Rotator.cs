58601852261c6aabd18ea569bd9a2e881383b64c
Gareth Lysnkey <lynskey123@gmail.com>
2016-03-30 23:58:56 +0100

    5th commit
    Added the asteroid and a rotation for it

---
+++ MobileApp/Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public float asteroidTumble;
+	
+	void Start () {
+
+		GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * asteroidTumble;
+
+	}
+}
