c91717a2edbd5087ee63b986b9e75ba588d24368
Gareth Lysnkey <lynskey123@gmail.com>
2016-03-31 00:13:21 +0100

    6th commit
    Asteroid and player collision detection and explosions

---
+++ MobileApp/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+
+		if (other.tag == "Player") 
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		Destroy(other.gameObject);//destroys the laser
+		Destroy(gameObject);//destroys the game object the script is attached to(asteroid)
+	}
+}
