9127bdb1e9e4580a36fe3afb9b27c31880eeecfe
Gareth Lysnkey <lynskey123@gmail.com>
2016-03-31 00:40:53 +0100

    12th commit
    Added an enemy ship, enemy laser and collision detection. The collision
    detection is still a bit buggy. I didn't use the original enemy ship i
    found as i kept running into problems/errors with it involving its
    collision detection, code for it that effected the lighting of the game
    and it disappearing in play mode.

--- MobileApp/Assets/Scripts/DestroyByContact.cs
+++ MobileApp/Assets/Scripts/DestroyByContact.cs
@@ -19,10 +19,18 @@ public class DestroyByContact : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Boundary"){
+		//find out which if statement works better: do research
+		//if (other.tag == ("Boundary") || other.tag == ("Enemy"))
+
+		if (other.CompareTag ("Boundary") || other.CompareTag ("Enemy"))
+		{
 			return;
+
 		}
-		Instantiate(explosion, transform.position, transform.rotation);
+
+		if (explosion != null){
+			Instantiate (explosion, transform.position, transform.rotation);
+		}
 
 		if (other.tag == "Player") {
 			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
@@ -31,7 +39,7 @@ public class DestroyByContact : MonoBehaviour {
 		else {
 			gameController.AddScore (scoreValue);
 		}
-			
+
 		Destroy(other.gameObject);//destroys the laser
 		Destroy(gameObject);//destroys the game object the script is attached to(asteroid)
 	}
---
+++ MobileApp/Assets/Scripts/EnemyWeaponController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWeaponController : MonoBehaviour {
+
+	public GameObject laser;
+	public Transform laserSpawn;
+	public float rateOfFire;
+	public float delay;
+
+	private AudioSource audioSource;
+
+	void Start ()
+	{
+		audioSource = GetComponent<AudioSource> ();
+		InvokeRepeating ("Fire", delay, rateOfFire);
+	}
+
+	void Fire ()
+	{
+		Instantiate (laser, laserSpawn.position, laserSpawn.rotation);
+		audioSource.Play ();
+	}
+}
